c6a5ae53bab0e5bb062d824f56395aab0431b491
Denis Nek <linkedin@nekdenis.com>
2017-07-14 10:06:49 +0800

    Initial commit

---
+++ app/src/main/java/com/kotlinsg/kworkshopapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.kotlinsg.kworkshopapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById(R.id.icon).setOnClickListener { openGithubScreen() }
+    }
+
+    private fun openGithubScreen() {
+        startActivity(Intent(this, GithubActivity::class.java))
+    }
+}
